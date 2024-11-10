1ba94d2cf7eecb77755e0cae2956ee90814da491
RamiMetry <99014901+RamiMetry@users.noreply.github.com>
2024-11-11 01:14:38 +0200

    main commit

---
+++ src/app/shop/page.jsx
@@ -0,0 +1,20 @@
+import ShopBreadcrumb from "@/components/breadcrumb/shop-breadcrumb";
+import ShopArea from "@/components/shop/shop-area";
+import Footer from "@/layout/footers/footer";
+import HeaderTwo from "@/layout/headers/header-2";
+import Wrapper from "@/layout/wrapper";
+
+export const metadata = {
+  title: "Cuideo Bassil Home - Shop Page",
+};
+
+export default function ShopPage() {
+  return (
+    <Wrapper>
+      <HeaderTwo style_2={true} />
+      <ShopBreadcrumb title="Shop Grid" subtitle="Shop Grid" />
+      <ShopArea/>
+      <Footer primary_style={true} />
+    </Wrapper>
+  );
+}
